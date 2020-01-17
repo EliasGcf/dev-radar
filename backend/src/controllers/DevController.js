d684a8b2d9e292c9c34f9d6d0010ac53c4c2aaf3
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-01-17 10:51:45 -0300

    Utilizando webSocket

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 // index, show, store, update, destroy
 
@@ -31,6 +32,17 @@ module.exports = {
 				techs: techsArray,
 				location,
 			});
+
+			// Filtrar as conexões que estão há no máximo 10km de distância
+			// e que o novo dev tenha pelo menos uma das tecnologias filtradas
+
+			const sendSocketMessageTo = findConnections(
+				{ latitude, longitude },
+				techsArray,
+			);
+
+			sendMessage(sendSocketMessageTo, 'new-dev', dev);
+
 		}
 
 		return res.json(dev);
